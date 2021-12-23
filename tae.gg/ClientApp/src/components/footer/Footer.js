0c3b95a6bfc07d8032ac2e7423d6a668b1534074
marcusbugge <marcus_bugge@hotmail.com>
2021-12-23 02:25:12 +0100

    Footer with ish-working links, league page and some frontpage styling

---
+++ tae.gg/ClientApp/src/components/footer/Footer.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "./footer.css";
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div className="footer-cnt">
+        <div className="footer-content">
+          <div className="staff-cnt">
+            <h1>TAE ESPORTS</h1>
+            <div className="stripe-mini"></div>
+
+            <div className="games">
+              <Link to="/games/valorant">News</Link>
+              <Link to="/games/leagueoflegends">About us</Link>
+              <Link to="/games/csgo">Staff</Link>
+            </div>
+          </div>
+          <div className="games-cnt">
+            <h1>OUR TEAMS</h1>
+            <div className="stripe-mini"></div>
+
+            <div className="games">
+              <Link to="/games/valorant">Valorant</Link>
+              <Link to="/games/leagueoflegends">League of Legends</Link>
+              <Link to="/games/csgo">Counter Strike: Global Offensive</Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="soscial-cnt">hahah</div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ tae.gg/ClientApp/src/components/pages/lol/League.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Header from "./Header";
+import "./lol.css";
+
+export class League extends Component {
+  render() {
+    return (
+      <div className="content-lol">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default League;
